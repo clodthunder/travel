d09a85c0fbc3fb928ae452f4b488e67f4718fef1
tzw <clodthunder@gmail.com>
2025-04-23 19:30:50 +0800

    项目初始化

---
+++ src/main/java/com/balance/travel/TravelApplication.java
@@ -0,0 +1,14 @@
+package com.balance.travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(TravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/balance/travel/TravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balance.travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
